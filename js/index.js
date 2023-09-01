f5b7184c4ce6982c5f85b62a7cfbd6931b9ce2a0
MAHAK <mahak191713@gmail.com>
2023-09-01 16:19:35 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -1,31 +1,30 @@
 var count = 0;
-var time = 0;
-var count2 = 0;
+var sec = 0;
+var min = 0;
 var Hr = 0;
 
 function start() {
     setInterval(() => {
         if (count < 360) {
             count = count + 6;
-            time = count / 6;
-            if (time === 60) {
-                count2 = count2 + 1;
+            sec = count / 6;
+            if (sec === 60) {
+                min = min + 1;
+            if (min === 60) {
+                Hr = Hr + 1;
+                min = min-60;
+            if(Hr===24){
+                Hr=Hr-24;
             }
-            if (count2 === 60) {
-                Hr =Hr+1;
             }
-            if(Hr === 24){
-                Hr =Hr-24;
-                count2 =count2-60;
-
-
-            }
+        }
+          
         }
     
         else {
             count=0;
         }
         document.getElementById('watch').style.backgroundImage = `conic-gradient(transparent 0deg, transparent ${count}deg, azure ${count}deg, azure)`
-        document.getElementById('time').innerHTML = ` ${Hr}hrs: ${count2}Min : ${time} Sec`;
+        document.getElementById('time').innerHTML = ` ${Hr}Hrs: ${min}Min : ${sec} Sec`;
 }, 1000)
 }
